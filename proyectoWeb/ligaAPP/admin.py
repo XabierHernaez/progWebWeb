3ade2721a6d7e43f82401c38c6da58a929c2672c
XabierHernaez <xabier.h.s@opendeusto.es>
2024-11-08 17:46:00 +0100

    commit

--- proyectoWeb/ligaAPP/admin.py
+++ proyectoWeb/ligaAPP/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Liga, Equipo, Jugador
 
 # Register your models here.
+
+admin.site.register(Liga)
+admin.site.register(Equipo)
+admin.site.register(Jugador)
--- proyectoWeb/ligaAPP/models.py
+++ proyectoWeb/ligaAPP/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+
+class Liga(models.Model):
+    nombre = models.CharField(max_length=50)
+    pais = models.CharField(max_length=50)
+    anio = models.DateField()
+    maximoGoleadorLiga = models.CharField(max_length=25)
+    maximoAsistenteLiga = models.CharField(max_length=50)
+    presidenteLiga = models.CharField(max_length=50)
+
+class Equipo(models.Model):
+    nombre = models.CharField(max_length=50)
+    ciudad = models.CharField(max_length=50)
+    maximoGoleadorEquipo = models.CharField(max_length=50)
+    maximoAsistenteEquipo = models.CharField(max_length=50)
+    AnioCreacion = models.DateField()
+    liga = models.ForeignKey(Liga, related_name = 'equipos', on_delete=models.CASCADE)
+
+class Jugador(models.Model):
+    nombre = models.CharField(max_length=50)
+    apellidos = models.CharField(max_length=50)
+    goles = models.CharField(max_length=50)
+    posicion = models.CharField(max_length=50)
+    anioDeUnion = models.DateField()
+    fechaNacimiento = models.DateField()
+    equipo = models.ForeignKey(Equipo, related_name = 'jugadores', on_delete=models.CASCADE)
