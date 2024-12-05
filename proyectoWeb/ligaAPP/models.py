da844cde52ad0bb342571a1a5e5fb010f1924842
ImanolSuarez <imanol.suarez@opendeusto.es>
2024-12-05 18:04:37 +0100

    Añadida nacionalidad

--- proyectoWeb/ligaAPP/models.py
+++ proyectoWeb/ligaAPP/models.py
@@ -11,6 +11,9 @@ class Liga(models.Model):
     presidenteLiga = models.CharField(max_length=50)
     imagenLiga = models.URLField(max_length=600, null= True, blank=True)
 
+    def __str__(self):
+        return self.nombre
+
 class Equipo(models.Model):
     nombre = models.CharField(max_length=50)
     ciudad = models.CharField(max_length=50)
@@ -20,13 +23,20 @@ class Equipo(models.Model):
     liga = models.ForeignKey(Liga, related_name = 'equipos', on_delete=models.CASCADE)
     imagenEquipo = models.URLField(max_length=600, null= True, blank=True)
 
+    def __str__(self):
+        return self.nombre
+
 
 class Jugador(models.Model):
     nombre = models.CharField(max_length=50)
     apellidos = models.CharField(max_length=50)
     goles = models.CharField(max_length=50)
     posicion = models.CharField(max_length=50)
+    nacionalidad = models.CharField(max_length=50, null= True, blank=True)
     anioDeUnion = models.DateField()
     fechaNacimiento = models.DateField()
     equipo = models.ForeignKey(Equipo, related_name = 'jugadores', on_delete=models.CASCADE)
     imagenJugador = models.URLField(max_length=600, null= True, blank=True)
+
+    def __str__(self):
+        return self.nombre + self.apellidos
